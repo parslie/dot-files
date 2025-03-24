1fb408142c57edd2b7cf675cc81a74b8d0e4a3b6
Viktor Holta <viktor.holta@outlook.com>
2025-03-24 08:48:13 +0100

    AGS: Show battery state and percentage

--- .config/ags/widget/bar/tray/Battery.tsx
+++ .config/ags/widget/bar/tray/Battery.tsx
@@ -1,3 +1,35 @@
+import AstalBattery from "gi://AstalBattery?version=0.1";
+import TrayButton from "./Button";
+import { bind } from "astal";
+import Gtk from "gi://Gtk?version=4.0";
+
+const battery = AstalBattery.get_default();
+
 export default function TrayBattery() {
-  return <menubutton></menubutton>;
+  return battery.isPresent ? (
+    <TrayButton iconName={bind(battery, "batteryIconName")}>
+      <box orientation={Gtk.Orientation.VERTICAL} widthRequest={300}>
+        <box>
+          <levelbar value={bind(battery, "percentage")} hexpand />
+          <label
+            label={bind(battery, "percentage").as(
+              (percentage) => `${(percentage * 100).toFixed(0)}%`,
+            )}
+          />
+        </box>
+        {bind(battery, "charging").as((charging) => (
+          <label
+            halign={Gtk.Align.START}
+            label={
+              charging
+                ? bind(battery, "timeToFull").as((timeToFull) => `${timeToFull} s until full`)
+                : bind(battery, "timeToEmpty").as((timeToEmpty) => `${timeToEmpty} s until empty`)
+            }
+          />
+        ))}
+      </box>
+    </TrayButton>
+  ) : (
+    <></>
+  );
 }
