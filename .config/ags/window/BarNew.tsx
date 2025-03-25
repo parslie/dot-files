8bd6fcd44d9c7c2ec9a1423b0c9d787a81a5d561
Viktor Holta <viktor.holta@outlook.com>
2025-03-25 05:43:30 +0100

    AGS: Init new bar

---
+++ .config/ags/window/BarNew.tsx
@@ -0,0 +1,29 @@
+import { App } from "astal/gtk4";
+import Astal from "gi://Astal?version=4.0";
+import Gdk from "gi://Gdk?version=4.0";
+import Gtk from "gi://Gtk?version=4.0";
+import Clock from "../widget/bar_new/Clock";
+
+export const BAR_NEW_NAME = "Bar New";
+
+export default function BarNew(monitor: Gdk.Monitor) {
+  return (
+    <window
+      visible
+      name={BAR_NEW_NAME}
+      application={App}
+      gdkmonitor={monitor}
+      cssClasses={["bar-new"]}
+      exclusivity={Astal.Exclusivity.EXCLUSIVE}
+      anchor={Astal.WindowAnchor.TOP | Astal.WindowAnchor.LEFT | Astal.WindowAnchor.RIGHT}
+    >
+      <centerbox cssName="centerbox">
+        <box></box>
+        <box>
+          <Clock />
+        </box>
+        <box></box>
+      </centerbox>
+    </window>
+  );
+}
--- .config/ags/window/index.ts
+++ .config/ags/window/index.ts
@@ -1,6 +1,7 @@
 import Bar from "./Bar";
+import BarNew from "./BarNew";
 import PowerMenu from "./PowerMenu";
 
-const windows = [Bar, PowerMenu];
+const windows = [BarNew, PowerMenu];
 
 export default windows;
