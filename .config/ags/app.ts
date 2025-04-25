973f60308a31cc2affcd90c663f62cb7ffe8799c
Viktor Holta <viktor.holta@outlook.com>
2025-04-25 19:55:21 +0200

    AGS: Add bar window with planned widgets

--- .config/ags/app.ts
+++ .config/ags/app.ts
@@ -2,9 +2,11 @@ import { App } from "astal/gtk4";
 import style from "./styles/index.scss";
 import Gdk from "gi://Gdk?version=4.0";
 import WidgetShowcase from "./windows/WidgetShowcase";
+import Bar from "./windows/Bar";
 
 function populate_monitor(monitor: Gdk.Monitor) {
   WidgetShowcase(monitor);
+  Bar(monitor);
 }
 
 App.start({
---
+++ .config/ags/windows/Bar.tsx
@@ -0,0 +1,34 @@
+import Astal from "gi://Astal?version=4.0";
+import Gdk from "gi://Gdk?version=4.0";
+
+export default function Bar(monitor: Gdk.Monitor) {
+  return (
+    <window
+      gdkmonitor={monitor}
+      keymode={Astal.Keymode.NONE}
+      layer={Astal.Layer.TOP}
+      exclusivity={Astal.Exclusivity.EXCLUSIVE}
+      anchor={Astal.WindowAnchor.RIGHT | Astal.WindowAnchor.BOTTOM | Astal.WindowAnchor.LEFT}
+      namespace={"bar"}
+      cssClasses={["bar"]}
+      visible
+    >
+      <centerbox cssName="centerbox">
+        <box>
+          <label label="[Power]" />
+          <label label="[Workspaces]" />
+        </box>
+        <box>
+          <label label="[Clock]" />
+        </box>
+        <box>
+          <label label="[Tray]" />
+          <label label="[Bluetooth]" />
+          <label label="[Network]" />
+          <label label="[Volume]" />
+          <label label="[Battery]" />
+        </box>
+      </centerbox>
+    </window>
+  );
+}
