6308df05d792a5fd8fef2e7d5d1a8c7d2acdfe46
Viktor Holta <viktor.holta@outlook.com>
2025-05-12 07:48:45 +0200

    Fish: Start UWSM on login

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,9 @@
+if status is-login
+	if uwsm check may-start; and uwsm select
+		exec uwsm start default
+	end
+end
+
 if status is-interactive
 	alias dot="git --git-dir=$HOME/.dot --work-tree=$HOME"
 end
